baadac81c9f0369d7e565a7fc670579cadda4cb1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-12-08 14:21:00 +0100

    implement tests

---
+++ run-in-docker-from-git.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# USAGE: ./run-from-git.sh https://gitlab.com/danylo.zhalkovskyy/node_app-web.git
+
+REPO_URL="${1}" # e.g. "https://gitlab.com/danylo.zhalkovskyy/node_app-web.git"
+PORT=3000
+
+echo "Generate Dockerfile from ${REPO_URL}..."
+cat > Dockerfile << CONTENTS
+FROM node:10
+WORKDIR /usr/src/app
+RUN git clone ${REPO_URL} .
+RUN npm install
+EXPOSE ${PORT}
+CMD [ "npm", "start" ]
+CONTENTS
+
+echo "Build and run Dockerfile..."
+docker build -t my-nodejs-app .
+
+echo ""
+echo "(Press Ctrl-C to exit)"
+docker run -t --rm --name my-running-app -p ${PORT}:${PORT} my-nodejs-app
+
+echo ""
+rm Dockerfile && echo "✅ Deleted the generated Dockerfile."
--- test.js
+++ test.js
@@ -1,10 +1,56 @@
 const test = require('ava');
+const axios = require('axios');
+const childProcess = require('child_process');
 
-test('foo', t => {
-  t.pass();
+test.before('Lecture du code source fourni', t => {
+  t.context.serverFiles = childProcess
+    .execSync('docker exec -i my-running-app sh -c "ls"')
+    .toString();
+  t.log(t.context.serverFiles);
+  t.context.serverSource = childProcess
+    .execSync('docker exec -i my-running-app sh -c "cat server.js"')
+    .toString();
+  t.log(t.context.serverSource);
 });
 
-test('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
+test('le code source ne contient pas plus de 5 fichiers', t => {
+  const nbLines = t.context.serverFiles.split('\n').length - 1;
+  t.assert(nbLines <= 5);
 });
+
+test('server.js fait moins de 30 lignes', t => {
+  const nbLines = t.context.serverSource.split('\n').length - 1;
+  t.assert(nbLines <= 30);
+});
+
+const suite = [
+  {
+    req: ['GET', '/'],
+    exp: '"Hello World"',
+    fct: (t, { data }) => t.true(data.includes('Hello World'))
+  },
+  {
+    req: ['GET', '/hello'],
+    exp: '"Quel est votre nom ?"',
+    fct: (t, { data }) => t.true(data.includes('Quel est votre nom ?'))
+  },
+  {
+    req: ['GET', '/hello?nom=Sasha'],
+    exp: '"Bonjour, Sasha"',
+    fct: (t, { data }) => t.true(data.includes('Bonjour, Sasha'))
+  },
+  {
+    req: ['GET', '/hello?nom=Patrick'],
+    exp: '"Bonjour, Patrick"',
+    fct: (t, { data }) => t.true(data.includes('Bonjour, Patrick'))
+  }
+];
+
+suite.forEach(testObj =>
+  test(`${testObj.req.join(' ')} retourne ${testObj.exp}`, async t => {
+    const method = testObj.req[0].toLowerCase();
+    const url = `http://localhost:3000${testObj.req[1]}`;
+    const res = await axios[method](url);
+    return testObj.fct(t, res);
+  })
+);
