13cdf3ac7f70710807640d68731b0bb6cd187b26
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-04 16:10:31 +0200

    fix: also remove timings in milliseconds

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 SOLUTIONS_REPO="https://adrienjoly:${GH_TOKEN}@github.com/adrienjoly/cours-nodejs-exercise-solutions.git"
 
 mkdir test_results
-TESTER=test-ex-1-3.js GIT_BRANCH=ex-1-3 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s,[0-9][0-9]*\.[0-9]*s,,g" >test_results/1-3.txt # exercices 1 à 3 (support de paramètres `GET`)
-TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s,[0-9][0-9]*\.[0-9]*s,,g" >test_results/1-5.txt # exercices 4 (support de paramètres `POST`) et 5 (persistance dans `réponses.json`)
+TESTER=test-ex-1-3.js GIT_BRANCH=ex-1-3 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s,[0-9][0-9]*\.*[0-9]*m*s,,g" >test_results/1-3.txt # exercices 1 à 3 (support de paramètres `GET`)
+TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s,[0-9][0-9]*\.*[0-9]*m*s,,g" >test_results/1-5.txt # exercices 4 (support de paramètres `POST`) et 5 (persistance dans `réponses.json`)
 
-# Note: sed "s,[0-9][0-9]*\.[0-9]*s,,g" is to remove durations/timings from npm and ava
+# Note: sed "s,[0-9][0-9]*\.*[0-9]*m*s,,g" is to remove durations/timings expressed in seconds or milliseconds, from npm and ava
 
 # TODO: compare with expected results and return non-zero error code in case of failure
