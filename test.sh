c84802a407f1abffd0810282d2fe9f53d43d299b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-04 17:59:27 +0200

    refactor: simplify test script

--- test.sh
+++ test.sh
@@ -1,13 +1,24 @@
 SOLUTIONS_REPO="https://adrienjoly:${GH_TOKEN}@github.com/adrienjoly/cours-nodejs-exercise-solutions.git"
 
 mkdir -p test_results >/dev/null
-TESTER=test-ex-1-3.js GIT_BRANCH=ex-1-3 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s, (*[0-9][0-9]*\.*[0-9]*m*s)*,,g" >test_results/1-3.txt # exercices 1 à 3 (support de paramètres `GET`)
-TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s, (*[0-9][0-9]*\.*[0-9]*m*s)*,,g" >test_results/1-5.txt # exercices 4 (support de paramètres `POST`) et 5 (persistance dans `réponses.json`)
 
-# Note: sed "s,[0-9][0-9]*\.*[0-9]*m*s,,g" is to remove durations/timings expressed in seconds or milliseconds, from npm and ava
+test () {
+  echo ""
+  echo "___ Testing ${GIT_BRANCH} ___"
+  ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} \
+    | sed "s, (*[0-9][0-9]*\.*[0-9]*m*s)*,,g" \
+    > test_results/${GIT_BRANCH}.txt
+  # Note: we use sed to remove durations/timings expressed in seconds or milliseconds, from npm and ava
 
-# Compare the output with the golden file (i.e. expected output)
-git --no-pager diff
+  # Now, let's compare the output with the golden file (i.e. expected output)
+  git --no-pager diff test_results/${GIT_BRANCH}.txt
+  echo ""
+}
 
-# Return a non-zero error code if the output has changed
+# Test suite
+
+TESTER=test-ex-1-3.js GIT_BRANCH=ex-1-3 test # exercices 1 à 3 (support de paramètres `GET`)
+TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 test # exercices 4 (support de paramètres `POST`) et 5 (persistance dans `réponses.json`)
+
+# Return a non-zero error code if any output file has changed
 exit $(git diff | wc -l)
