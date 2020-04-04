f4f3ae450ebb848f6a40724d8b864dda7412f2b7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-04 17:45:39 +0200

    fix(test): clean test logs + make git diff non interactive

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 SOLUTIONS_REPO="https://adrienjoly:${GH_TOKEN}@github.com/adrienjoly/cours-nodejs-exercise-solutions.git"
 
-mkdir test_results
+mkdir -p test_results >/dev/null
 TESTER=test-ex-1-3.js GIT_BRANCH=ex-1-3 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s, (*[0-9][0-9]*\.*[0-9]*m*s)*,,g" >test_results/1-3.txt # exercices 1 à 3 (support de paramètres `GET`)
 TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} | sed "s, (*[0-9][0-9]*\.*[0-9]*m*s)*,,g" >test_results/1-5.txt # exercices 4 (support de paramètres `POST`) et 5 (persistance dans `réponses.json`)
 
 # Note: sed "s,[0-9][0-9]*\.*[0-9]*m*s,,g" is to remove durations/timings expressed in seconds or milliseconds, from npm and ava
 
 # Compare the output with the golden file (i.e. expected output)
-git diff
+git --no-pager diff
 
 # Return a non-zero error code if the output has changed
 exit $(git diff | wc -l)
