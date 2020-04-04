21adee9eb8a983fd6ff4003b465da5a8e7aa2369
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-04 15:44:46 +0200

    fix: run ava from script => `$ npm test` will test this repo's code instead

--- test-in-docker-from-dir.sh
+++ test-in-docker-from-dir.sh
@@ -36,7 +36,8 @@ set +e # from now on, keep running the script, even if a command returns a non-z
 
 echo ""
 echo "Run test suite against container..."
-npm test 2>&1 # ${TESTER} will be passed
+# npx ava ${TESTER:=test-ex-1-3.js} --serial --tap | npx faucet 2>&1
+npx ava ${TESTER:=test-ex-1-3.js} --serial -v 2>&1
 
 echo ""
 echo "Stop and remove Docker containers..."
