4271d61b83008098dcf5d2d4cb3515972b733ac4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-05 13:03:50 +0200

    failed tentative to run mongomem in docker container

--- test-ex-2-1.js
+++ test-ex-2-1.js
@@ -1,6 +1,6 @@
 const test = require('ava');
 const { runInDocker } = require('./runInDocker');
-const { MongoDBServer } = require('mongomem');
+const childProcess = require('child_process');
 
 // Préparation des tests
 
@@ -24,9 +24,53 @@ test.serial(
 // Exigences fonctionnelles
 
 test.serial('connect to mongodb from container', async t => {
-  await MongoDBServer.start();
-  const url = await MongoDBServer.getConnectionString();
-  console.log('connection string:', url);
+  t.timeout(2 * 60 * 1000); // 2 minutes
+
+  console.log('install mongomem...');
+  console.log(runInDocker(`npm install --no-audit mongomem babel-runtime`));
+
+  console.log('run mongo server in container...');
+  const serverCode = `
+  const { MongoDBServer } = require('mongomem');
+  (async () => {
+    console.log('--starting');
+    await MongoDBServer.start();
+    console.log('--getConnectionString');
+    const url = await MongoDBServer.getConnectionString();
+    console.log('connection string:', url);
+  })();
+  `;
+
+  const mongodbUri = await new Promise((resolve, reject) => {
+    const serverProcess = childProcess.exec(
+      `docker exec my-running-app node -e "${serverCode.replace(/\n/g, ' ')}"`
+    );
+    serverProcess.stdout.on('data', data => {
+      //if (!/\d\.\d \%/.test(data)) {
+      console.log(data);
+      //}
+      const done = data.toString().includes('connection string');
+      if (done) {
+        const connectionString = data
+          .toString()
+          .split(': ')
+          .pop();
+        console.log('done!', { connectionString });
+        resolve(connectionString);
+      }
+    });
+    serverProcess.stderr.on('data', data => {
+      reject(data);
+    });
+    serverProcess.stderr.on('error', data => {
+      reject(data);
+    });
+  });
+
+  console.log('run client code...');
+  // await MongoDBServer.start();
+  // const url = await MongoDBServer.getConnectionString();
+  // console.log('connection string:', url);
   const clientCode = `
   const MongoClient = require('mongodb').MongoClient;
   MongoClient.connect(process.env.MONGODB_URI, function(err, client) {
@@ -38,7 +82,7 @@ test.serial('connect to mongodb from container', async t => {
   `;
   runInDocker(`npm install --no-audit mongodb`);
   const result = runInDocker(
-    `MONGODB_URI="${url}" node -e "${clientCode.replace(/\n/g, ' ')}"`
+    `MONGODB_URI="${mongodbUri}" node -e "${clientCode.replace(/\n/g, ' ')}"`
   );
   console.log(result);
 });
