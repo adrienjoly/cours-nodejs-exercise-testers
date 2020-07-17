01103a0bfb7e28bd14e632826430cd324c23e16b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-17 11:20:55 +0200

    feat: also run test-partiel.sh from test.sh, for CI

--- test.sh
+++ test.sh
@@ -31,5 +31,7 @@ TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 test # exercices 4 (support de paramètr
 # Test suite: Deuxième partie du cours Node.js: Connexion base de données MongoDB (https://adrienjoly.com/cours-nodejs/02-db/)
 TESTER=test-ex-2-*.js GIT_BRANCH=ex-2-2 test # exercices 1 et 2
 
+./test-partiel.sh
+
 # Return a non-zero error code if any output file has changed
 exit $(git diff | wc -l)
