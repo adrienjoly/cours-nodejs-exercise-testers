2362963627e954b0a30572b666fb924a2554721d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-17 15:19:48 +0200

    test-partiels.sh runs the test in stdout

--- test-partiel.sh
+++ test-partiel.sh
@@ -1,3 +1,1 @@
-TESTER=test-partiel.js ./test-in-docker-from-dir.sh ./partiel-solution && \
-TESTER=test-partiel.js ./test-in-docker-from-dir.sh ./partiel-solution > test_results/test-partiel.txt
-exit $(git diff | wc -l)
+TESTER=test-partiel.js ./test-in-docker-from-dir.sh ./partiel-solution
