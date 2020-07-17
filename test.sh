2cf51cb79792d6636c3edd3231eea88fdf535b50
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-17 15:00:59 +0200

    fix(tests): ignore timings from output of ava tests, to prevent false alerts in diff

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ removeNodeProcessId () {
   sed "s,(node:[0-9][0-9]*) ,(node) ,g"
 }
 
-test () {
+testFromGit () {
   echo ""
   echo "___ Testing ${GIT_BRANCH} ___"
   ./test-in-docker-from-git.sh ${SOLUTIONS_REPO} \
@@ -24,14 +24,28 @@ test () {
   echo ""
 }
 
+testFromDir () {
+  echo ""
+  echo "___ Testing ./$1 ___"
+  ./test-in-docker-from-dir.sh $1 \
+    | removeTimings \
+    | removeNodeProcessId \
+    > test_results/$1.txt
+
+  # Now, let's compare the output with the golden file (i.e. expected output)
+  git --no-pager diff test_results/$1.txt
+  echo ""
+}
+
+# Test suite: Partiel 2A WebDev 2020
+TESTER=test-partiel.js testFromDir partiel-solution
+
 # Test suite: Première partie du cours Node.js: Chat-bot avec Express (https://adrienjoly.com/cours-nodejs/01-chatbot/)
-TESTER=test-ex-1-3.js GIT_BRANCH=ex-1-3 test # exercices 1 à 3 (support de paramètres `GET`)
-TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 test # exercices 4 (support de paramètres `POST`) et 5 (persistance dans `réponses.json`)
+TESTER=test-ex-1-3.js GIT_BRANCH=ex-1-3 testFromGit # exercices 1 à 3 (support de paramètres `GET`)
+TESTER=test-ex-1-5.js GIT_BRANCH=ex-1-5 testFromGit # exercices 4 (support de paramètres `POST`) et 5 (persistance dans `réponses.json`)
 
 # Test suite: Deuxième partie du cours Node.js: Connexion base de données MongoDB (https://adrienjoly.com/cours-nodejs/02-db/)
-TESTER=test-ex-2-*.js GIT_BRANCH=ex-2-2 test # exercices 1 et 2
-
-TESTER=test-partiel.js ./test-in-docker-from-dir.sh ./partiel-solution > test_results/test-partiel.txt
+TESTER=test-ex-2-*.js GIT_BRANCH=ex-2-2 testFromGit # exercices 1 et 2
 
 # Return a non-zero error code if any output file has changed
 exit $(git diff | wc -l)
